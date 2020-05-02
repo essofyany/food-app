013ad65e2fb12169bba99cadf0998b1102c9143c
essofyany <62668827+essofyany@users.noreply.github.com>
2020-05-02 02:25:23 +0000

    Add files via upload

---
+++ ui.js
@@ -0,0 +1,64 @@
+const data = [
+    {
+        image: '/img/9.jpeg'
+    },
+    {
+        image: '/img/1.jpeg'
+    },
+    {
+        image: '/img/6.jpeg'
+    },
+    {
+        image: '/img/3.jpeg'
+    },
+    {
+        image: '/img/d1.jpeg'
+    },
+    {
+        image: '/img/d9.jpeg'
+    },
+    {
+        image: '/img/d7.jpeg'
+    }
+];
+
+data.sort( ()=> Math.random() - 0.5);
+
+const images = document.querySelectorAll('.image')
+const imageList = Array.from(images);
+imageList.forEach((image, index)=> {
+    image.setAttribute('src',`${data[index].image}`) ;
+});
+
+// food & drink underline
+const foodDrink = document.querySelector('.food-drink');
+const foods = document.querySelector('.foods');
+const drinks = document.querySelector('.drinks');
+const underlineDisplay = 'text-decoration: underline green;';
+foodDrink.addEventListener('click', (e)=> {
+    if(e.target.textContent === foods.textContent) {
+        e.target.setAttribute('style', `${underlineDisplay}`);
+        drinks.setAttribute('style', 'none');
+    } else if(e.target.textContent === drinks.textContent) {
+        e.target.setAttribute('style', `${underlineDisplay}`);
+        foods.setAttribute('style', 'none');
+    }
+});
+
+// country choosen 
+const country = document.querySelectorAll('.country');
+const countryList = Array.from(country);
+countryList.forEach((country)=> {
+    country.addEventListener('click', (e)=> {
+        if(e.target.textContent === 'Other') {
+            document.querySelector('.recently-added').textContent = 'Recently Added';
+        } else {
+            document.querySelector('.recently-added').textContent = e.target.textContent;
+        }
+    })
+});
+
+// logo clicked
+document.querySelector('.brand-logo').addEventListener('click', ()=> {
+    location.reload();
+});
